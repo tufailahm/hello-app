017bfc3731dc120388e6545ba1c7a86ad6fcab6a
Tufail Ahmed <tufailahmedkhan@gmail.com>
2022-03-31 00:51:43 +0530

    second commit by Tufail

--- src/main/java/com/training/pms/HelloAppApplication.java
+++ src/main/java/com/training/pms/HelloAppApplication.java
@@ -16,4 +16,8 @@ public class HelloAppApplication {
 	public String sayHello() {
 		return "Hello My Friend";
 	}
+	@GetMapping("/sayBye")
+	public String sayBye() {
+		return "Bye My Friend";
+	}
 }
